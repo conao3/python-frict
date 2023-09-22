3397674d82a8100a18d67a3d9f14d295e78226fa
Naoya Yamashita <conao3@gmail.com>
2023-09-22 22:10:04 +0900

    implement first frict

--- src/frict/lib.py
+++ src/frict/lib.py
@@ -3,6 +3,8 @@ from typing import Any
 import typing
 
 
+previous_lines = 0
+
 def frict(
     *obj: Any,
     sep: str = '',
@@ -10,4 +12,10 @@ def frict(
     file: typing.IO[str] = sys.stdout,
     flush: bool = False,
 ):
+    global previous_lines
+
+    if previous_lines:
+        print(f'\033[{previous_lines}A\033[J', end='', file=file, flush=True)
+
+    previous_lines = sum((str(elm) + end).count('\n') for elm in obj)
     print(*obj, sep=sep, end=end, file=file, flush=flush)
